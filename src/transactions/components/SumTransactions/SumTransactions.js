2962f860af901d5473e257c5f80f5a1dbc82e3d4
Kamil Drazkiewicz <k.drazkiewicz@getprintbox.com>
2019-11-05 09:39:47 +0100

    add sum component logic

--- src/transactions/components/SumTransactions/SumTransactions.js
+++ src/transactions/components/SumTransactions/SumTransactions.js
@@ -2,9 +2,12 @@ import React from 'react'
 import {useSelector} from "react-redux";
 
 const SumTransactions = () => {
+	const transactions = useSelector(state => state.transactions.list);
+	const sumEuroAllTransactions = transactions.reduce( (sum, transaction) => sum + transaction.amountEuro, 0);
 	return (
 		<div>
 
+			{sumEuroAllTransactions}
 		</div>
 	)
 };
