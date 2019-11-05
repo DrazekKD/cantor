09639e73c5c92d04903d15a3bc1febb49b963fa8
Kamil Drążkiewicz <k.drazkiewicz@getprintbox.com>
2019-11-05 22:13:42 +0100

    Updates

---
+++ precache-manifest.418671571048dfe3a9bcbe81a3e26add.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "17acfee7fed6ce240092407941bf492e",
+    "url": "/DrazekKD/cantor/index.html"
+  },
+  {
+    "revision": "11961ac9d834392b5487",
+    "url": "/DrazekKD/cantor/static/css/2.1e6845a9.chunk.css"
+  },
+  {
+    "revision": "6a0cea59f2a7377ebfee",
+    "url": "/DrazekKD/cantor/static/css/main.047b4d98.chunk.css"
+  },
+  {
+    "revision": "11961ac9d834392b5487",
+    "url": "/DrazekKD/cantor/static/js/2.46664ad8.chunk.js"
+  },
+  {
+    "revision": "6a0cea59f2a7377ebfee",
+    "url": "/DrazekKD/cantor/static/js/main.8c02399f.chunk.js"
+  },
+  {
+    "revision": "b1f4e75d322ce922f55a",
+    "url": "/DrazekKD/cantor/static/js/runtime-main.26722e7c.js"
+  },
+  {
+    "revision": "2ce17e117605e8294f335116935a862f",
+    "url": "/DrazekKD/cantor/static/media/euro.2ce17e11.svg"
+  },
+  {
+    "revision": "cf322526d988e5ccbd6868a927aa255d",
+    "url": "/DrazekKD/cantor/static/media/zloty.cf322526.svg"
+  }
+]);
